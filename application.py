d009bba4b0e873ffbf1d3e7eea2f9815b4662b9a
Paul Warner <paul.eliot.warner@gmail.com>
2016-12-04 08:28:21 -0500

    New towin query

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-from flask import Flask, request, render_template, g
+from flask import Flask, request, render_template, g, session
 
 from create_database import create_database
 
@@ -54,7 +54,9 @@ def start_vote_percent():
     return start_support
 
 BILL_ID_QUERY = '''SELECT bill_id FROM bill_on WHERE id=?'''
-ROLLCALL_QUERY = '''SELECT roll_call_id, date, description FROM rollcalls WHERE bill_id=?'''
+ROLLCALL_QUERY = '''SELECT roll_call_id, date, description FROM rollcalls
+                    WHERE bill_id=?
+                    AND description LIKE "%Passed%"'''
 VOTES_QUERY = """SELECT sponsor_id, vote FROM votes WHERE roll_call_id=?"""
 ROLLCALL_COUNT_QUERY = '''SELECT COUNT(*) FROM votes where roll_call_id=?'''
 FAVORING_QUERY = '''SELECT COUNT(*) FROM votes WHERE roll_call_id=? AND vote=?'''
@@ -62,28 +64,39 @@ OPPOSING_QUERY = '''SELECT COUNT(*) FROM votes WHERE roll_call_id=? AND vote!=?'
 TERM_QUERY = '''SELECT district FROM term WHERE sponsor_id=? and start<=? and end>=?'''
 POPULATION_QUERY = '''SELECT population FROM districts WHERE district_id=?'''
 TOTAL_POPULATION_QUERY = '''SELECT SUM(population) FROM districts WHERE district_id LIKE ?'''
-# Get all rollcalls where the districts representative voted differently from the user: district, user vote
-REPRESENTED_QUERY = '''SELECT sponsor_id FROM sponsors s WHERE
-                       EXISTS(SELECT district_id FROM term WHERE sponsor_id=s.sponsor_id AND
-                                                              district_id=?)'''
-REP_BILLS_QUERY = '''SELECT * FROM bills b WHERE
-                     '''
-REP_VOTED_ON_QUERY = '''SELECT * FROM rollcalls r WHERE
-                        EXISTS(SELECT * FROM votes v where v.roll_call_id = r.roll_call_id and sponsor_id=?)
-                        '''
-BILL_FROM_ROLLCALL_QUERY = '''SELECT * from bills b WHERE b.bill_id IN(SELECT bill_id FROM rollcalls r WHERE
-                              EXISTS(SELECT * FROM votes v where v.roll_call_id = r.roll_call_id
-                              AND sponsor_id IN(SELECT sponsor_id FROM sponsors s WHERE
-                              EXISTS(SELECT district_id FROM term t WHERE t.sponsor_id=s.sponsor_id
-                              AND district_id=?))))'''
-BILLS_IN_QUERY = '''SELECT * FROM bills b WHERE 
-                    b.bill_id IN()
+DISTRICT_DIFFERENCE_QUERY = '''SELECT b.bill_id FROM bills b WHERE
+                    EXISTS(SELECT r.roll_call_id FROM rollcalls r WHERE
+                    b.bill_id=r.bill_id
+                    AND r.description LIKE "%Passed%" 
+                    AND EXISTS(SELECT v1.roll_call_id FROM votes v1, votes v2 WHERE 
+                        r.roll_call_id=v1.roll_call_id 
+                        AND v2.sponsor_id IN(SELECT sponsor_id FROM term t
+                                              WHERE v2.sponsor_id=t.sponsor_id
+                                              AND t.district=?)
+                        AND v1.sponsor_id IN(SELECT sponsor_id FROM term t
+                                            WHERE v1.sponsor_id=t.sponsor_id 
+                                            AND t.district=?) 
+                        AND v1.vote!=v2.vote))
                     AND b.bill_id IN(SELECT bill_id FROM bill_on)'''
+INSERT_USER_QUERY = '''INSERT INTO people VALUES(1, "USER")'''
+INSERT_USER_TERM_QUERY = '''INSERT INTO term(sponsor_id, district) VALUES(1,"USER")'''
+INSERT_VOTES_QUERY = '''INSERT INTO votes VALUES(?,?,?)'''
+DELETE_VOTES_QUERY = '''DELETE FROM votes WHERE sponsor_id=?'''
+DELETE_USER_QUERY = '''DELETE FROM people WHERE sponsor_id=?'''
+DELETE_TERM_QUERY = '''DELETE FROM term where sponsor_id=?'''
+SELECT_BILLS = '''SELECT question_text FROM bill_on WHERE bill_id=?'''
+VOTE_ON_QUERY = '''SELECT p.name, v.vote FROM people p, votes v WHERE
+                   p.sponsor_id=v.sponsor_id
+                   AND v.roll_call_id IN(SELECT roll_call_id FROM rollcalls r WHERE r.bill_id=? AND r.description LIKE "%Passed%")
+                   AND p.sponsor_id=?'''
+VOTE_FROM_QUERY = '''SELECT sponsor_id, vote FROM votes WHERE '''
+BILL_ON_QUERY = '''SELECT id FROM bill_on WHERE bill_id=?'''
 
 def generate_results(question_answers):
     db = get_db()
     vote_percents = start_vote_percent()
     senate, house = vote_percents
+    cantfind = set()
     for question, answer in question_answers.items():
         bill, = db.execute(BILL_ID_QUERY, (question,)).fetchone()
         rollcurs = db.execute(ROLLCALL_QUERY, (bill,))
@@ -91,14 +104,12 @@ def generate_results(question_answers):
             # print('no votes on bill {}'.format(bill))
             continue
         for rollcall, date, description in rollcurs:
-            if "Amendment" in description or "Failed" in description:
-                continue
             votes = db.execute(VOTES_QUERY, (rollcall,))
             for congressman, vote in votes:
                 try:
                     districtt, = db.execute(TERM_QUERY, (congressman, date, date))
                 except ValueError as err:
-                    # print("error unpacking values: {}".format(err))
+                    cantfind.add(congressman)
                     continue
                 district = districtt[0]
                 # count, = db.execute(ROLLCALL_COUNT_QUERY, (rollcall,)).fetchone()
@@ -122,12 +133,13 @@ def generate_results(question_answers):
         if len(house[district]):
             house[district] = sum(house[district])/len(house[district])
         else:
-            house[district] = 0
+            house[district] = .5
     for district in senate:
         if len(senate[district]):
             senate[district] = sum(senate[district])/len(senate[district])
         else:
-            senate[district] = 0
+            senate[district] = .5
+    print(cantfind)
     return vote_percents
 
 def calc_vote_total(vote_percent):
@@ -157,22 +169,48 @@ def main():
             question_answers[question_id] = int(request.form[answer])
         district_results = generate_results(question_answers)
         totals = calc_vote_total(district_results)
+        session['answers'] = question_answers
+        session['district_results'] = district_results
+        session['totals'] = totals
         return render_template('results.html', district_results=district_results, totals=totals)
 
-@application.route("/unlike", methods=['POST'])
-def unlike_queries():
+@application.route("/towin", methods=['POST'])
+def to_win():
+    if 'district' not in request.form:
+        return "Error: no district!"
+    if 'answers' not in session:
+        return "No active session!"
     db = get_db()
-    district_id = int(request.form['district_id'])
-    represented = db.execute(REPRESENTED_QUERY, (district_id,))
-    for sponsor in represented:
-        pass
-
-
+    district = request.form['district']
+    question_answers = session['answers']
+    db.execute(INSERT_USER_QUERY)
+    db.execute(INSERT_USER_TERM_QUERY)
+    for question, answer in question_answers.items():
+        bill, = db.execute(BILL_ID_QUERY, (question,)).fetchone()
+        rollcurs = db.execute(ROLLCALL_QUERY, (bill,))
+        if not rollcurs.rowcount:
+            continue
+        for rollcall, _, _ in rollcurs:
+            db.execute(INSERT_VOTES_QUERY, (rollcall, 1, answer))
+    bills = db.execute(DISTRICT_DIFFERENCE_QUERY, ("USER", district))
+    db.execute(DELETE_TERM_QUERY, (1,))
+    db.execute(DELETE_VOTES_QUERY, (1,))
+    db.execute(DELETE_USER_QUERY, (1,))
+    result = []
+    for bill, in bills:
+        question, = db.execute(SELECT_BILLS, (bill,)).fetchone()
+        bill_on, = db.execute(BILL_ON_QUERY, (bill,)).fetchone()
+        my_answer = question_answers[str(bill_on)]
+        result.append((question, 1 if my_answer == 2 else 1))
+    print(result)
+    return render_template('towin.html', bills=result)
 
 @application.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'db'):
         g.db.close()
+
+application.secret_key = "%s" % os.urandom(24)
 
 if __name__ == '__main__':
     print('database okay, running server...')
